282ab3a862cc3e8135cd57a97536ce099045d1e8
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2024-03-24 11:19:53 +0300

    styleMock.js added

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+      'swiper/css': '<rootDir>/__mocks__/styleMock.js',
+      'swiper/css/navigation': '<rootDir>/__mocks__/styleMock.js',
+      'swiper/css/pagination': '<rootDir>/__mocks__/styleMock.js',
+      'swiper/css/autoplay': '<rootDir>/__mocks__/styleMock.js',
+    },
     transformIgnorePatterns: [
       "node_modules/(?!(swiper|ssr-window|dom7)/)"
     ],
+   
   };
   
--- src/components/Products.test.js
+++ src/components/Products.test.js
@@ -1,77 +1,63 @@
 import React from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import axios from 'axios';
-import Products from '../components/Products';
+import Products from './Products';
+import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('axios');
 
 const mockProducts = [
-  { id: 1, title: 'Ürün 1', price: 10, image: '/image1.jpg', category: 'electronics' },
-  { id: 2, title: 'Ürün 2', price: 20, image: '/image2.jpg', category: 'jewelery' },
+  { id: 1, title: 'Ürün 1', price: '9.99', image: '/image1.jpg', category: 'electronics' },
+  { id: 2, title: 'Ürün 2', price: '20.99', image: '/image2.jpg', category: 'jewelery' },
 ];
 
 describe('Products Component Tests', () => {
-    test('products are fetched and displayed after loading', async () => {
-        axios.get.mockResolvedValue({ data: mockProducts });
-        render(<Products />);
-        expect(screen.getByText(/loading/i)).toBeInTheDocument();
-        await waitFor(() => {
-          const productElements = screen.getAllByText(/Ürün/);
-          expect(productElements).toHaveLength(mockProducts.length);
-        });
-      });
-      
+  beforeEach(async () => {
+    axios.get.mockResolvedValue({ data: mockProducts });
+  });
 
-      test('products are filtered by category', async () => {
-        axios.get.mockResolvedValue({ data: mockProducts });
-        render(<Products />);
-        
-        await waitFor(() => {
-          expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
-        });
-        
-        const select = screen.getByRole('combobox');
-        fireEvent.change(select, { target: { value: 'electronics' } });
-      
-        // 'Ürün 1'in ekranda olduğunu kontrol et
-        await waitFor(() => {
-          expect(screen.getByText('Ürün 1')).toBeInTheDocument();
-        });
-      
-        // 'Ürün 2'nin ekranda olmadığını kontrol et
-        await waitFor(() => {
-          expect(screen.queryByText('Ürün 2')).not.toBeInTheDocument();
-        });
-      });
-      
+  test('products are fetched and displayed after loading', async () => {
+    render(<Products />);
+    await waitFor(() => {
+      const productElements = screen.getAllByText(/Ürün/);
+      expect(productElements).toHaveLength(mockProducts.length);
+    });
+  });
 
-      test('all products are displayed when "All" category is selected', async () => {
-        axios.get.mockResolvedValue({ data: mockProducts });
-        render(<Products />);
-        await waitFor(() => {
-          expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
-        });
-      
-        const select = screen.getByRole('combobox');
-        fireEvent.change(select, { target: { value: 'All' } });
-      
-        // 'Ürün 1'in ekranda olduğunu kontrol et
-        await waitFor(() => {
-          expect(screen.getByText('Ürün 1')).toBeInTheDocument();
-        });
-      
-        // 'Ürün 2'nin ekranda olduğunu kontrol et
-        await waitFor(() => {
-          expect(screen.getByText('Ürün 2')).toBeInTheDocument();
-        });
-      });
-      
+  test('products are filtered by category', async () => {
+    render(<Products />);
+    const select = screen.getByRole('combobox');
+    fireEvent.change(select, { target: { value: 'electronics' } });
+
+    await waitFor(() => {
+      expect(screen.getByText('Ürün 1')).toBeInTheDocument();
+    });
+
+    // Ayrı waitFor kullanımı
+    await waitFor(() => {
+      expect(screen.queryByText('Ürün 2')).not.toBeInTheDocument();
+    });
+  });
+
+  test('all products are displayed when "All" category is selected', async () => {
+    render(<Products />);
+    const select = screen.getByRole('combobox');
+    fireEvent.change(select, { target: { value: 'All' } });
+
+    // Her beklenen element için ayrı waitFor kullanımı
+    await waitFor(() => {
+      expect(screen.getByText('Ürün 1')).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('Ürün 2')).toBeInTheDocument();
+    });
+  });
 });
 
 describe('Products Component Snapshot Test', () => {
-    // Snapshot test 
-    it('should match the snapshot', () => {
-      const { asFragment } = render(<Products />);
-      expect(asFragment()).toMatchSnapshot();
-    });
+  it('should match the snapshot', () => {
+    const { asFragment } = render(<Products />);
+    expect(asFragment()).toMatchSnapshot();
   });
+});
